fff8e365242ffae4ad4a102ea1e0e3df6c228ef4
Jacob Searles <jacob.searles@student.sl.on.ca>
2025-11-11 09:23:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,25 @@
             });
         })
         .catch(error => console.error('Error fetching data:', error));
+
+        // Search Bar function ;)
+
+        function searchTable() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("search-bar");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("data-table");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[0];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
